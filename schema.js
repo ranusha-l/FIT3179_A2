9c807029f431a3b5b9c71f1724197d62f5c7f4b2
ranusha-l <rliy0007@student.monash.edu>
2024-10-15 23:47:45 +0800

    Final Commit

--- schema.js
+++ schema.js
@@ -3,7 +3,37 @@ var vg_2 = "line_chart.vg.json";
 var vg_3 = "bar_chart.vg.json";
 var vg_4 = "pie_chart.vg.json";
 
+
+
+function updateYear(year) {
+    selectedYear = year;
+    document.getElementById("yearLabel").textContent = year; // Update label
+    vegaEmbed('#map', vg_1).then(function(result) {
+        result.view.signal('selectedYear', selectedYear).run();
+
+        result.view.signal('selectedSector', selectedSector).run();
+        }).catch(console.error);
+
+    vegaEmbed('#bar_chart', vg_3).then(function(result) {
+        result.view.signal('selectedYear', selectedYear).run();
+        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+        }).catch(console.error);
+    
+    vegaEmbed('#bar_chart', vg_3).then(function(result) {
+        result.view.signal('selectedYear', selectedYear).run();
+        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+        }).catch(console.error);
+    
+    vegaEmbed('#pie_chart', vg_4).then(function(result) {
+        result.view.signal('selectedYear', selectedYear).run();
+        result.view.signal('selectedState', selectedState).run();
+        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+        }).catch(console.error);
+
+  }
+
 vegaEmbed('#map', vg_1).then(function(result) {
+
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
 
